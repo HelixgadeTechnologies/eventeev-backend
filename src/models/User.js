c159b08dbca10a97fcb523baed720c6a593fe738
alphadude <alphadude4all@gmail.com>
2026-04-06 22:28:57 +0100

    refactor: simplify UserSchema password hashing middleware by removing unused next callback

--- src/models/User.js
+++ src/models/User.js
@@ -48,9 +48,9 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Encrypt password using bcrypt
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   if (!this.isModified('password') || !this.password) {
-    next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
