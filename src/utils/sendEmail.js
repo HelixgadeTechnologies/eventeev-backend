31fd88c967e66c10918c65d8fbfeb6114bd947c7
alphadude <alphadude4all@gmail.com>
2026-05-07 21:57:49 +0100

    fix: update nodemailer configuration to use Gmail service with STARTTLS and forced IPv4 connection

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,20 +2,22 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
+    service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_APP_PASS,
     },
     tls: {
       rejectUnauthorized: false,
+      // Force IPv4
+      family: 4
     },
-    connectionTimeout: 10000, // 10 seconds
+    connectionTimeout: 10000, 
     greetingTimeout: 10000,
     socketTimeout: 10000,
-    family: 4,
   });
 
   const message = {
